45f97190c3d834c6a0cdca96d35fb7f154c8bed8
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-06-12 20:39:08 +0530

    Initial commit

---
+++ models/src/main/java/com/example/models/CartItem.java
@@ -0,0 +1,25 @@
+package com.example.models;
+
+public class CartItem {
+
+    public  String name;
+    Float UnitPerPrice , qty;
+
+    public CartItem(String name, Float unitPerPrice, Float qty) {
+        this.name = name;
+        this.UnitPerPrice = unitPerPrice;
+        this.qty = qty;
+    }
+
+    Float cost(){
+        return UnitPerPrice * qty;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "name='" + name + '\'' +
+                String.format("\n,cost= %f X %f = %f",UnitPerPrice,qty,cost())
+                +'}';
+    }
+}
